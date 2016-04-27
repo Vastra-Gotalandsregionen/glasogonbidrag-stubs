0c3b70f390bd94c9955205b6010841d2dd39beea
mrg3kko <erik.andersson@monator.com>
2016-04-27 17:40:19 +0200

    Stubs

--- core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/backingbean/RegisterGrantBackingBean.java
+++ core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/backingbean/RegisterGrantBackingBean.java
@@ -1,5 +1,6 @@
 package se.vgregion.glasogonbidrag.backingbean;
 
+import com.liferay.faces.portal.context.LiferayFacesContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
@@ -9,38 +10,58 @@ import se.vgregion.glasogonbidrag.vo.IssuedGrantVO;
 import se.vgregion.glasogonbidrag.vo.PersonVO;
 
 import javax.annotation.PostConstruct;
-import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.context.FacesContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component(value = "registerGrantBackingBean")
 @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class RegisterGrantBackingBean {
 
+    private int tabIndexActive;
     private GrantVO grant;
     private PersonVO person;
     private List<IssuedGrantVO> issuedGrants;
+    private String portletNamespace;
 
     @PostConstruct
     public void init() {
 
         System.out.println("RegisterGrantBackingBean - init");
 
+        portletNamespace = FacesContext.getCurrentInstance().getExternalContext().encodeNamespace("");
         grant = new GrantVO();
         person = null;
         issuedGrants = new ArrayList<IssuedGrantVO>();
+
+        tabIndexActive = 0;
+
     }
 
-    public void personNumberChange() {
 
-        person = GrantUtil.getPerson(grant.getPersonNumber());
-        issuedGrants = GrantUtil.getIssuedGrants(person.getPersonNumber());
+    public void tabsListener(int tabIndexNew){
+        System.out.println("RegisterGrantBackingBean - tabsListener");
+
+        tabIndexActive = tabIndexNew;
     }
 
-    public final void grantTypeChange(final AjaxBehaviorEvent event){
-        System.out.println("RegisterGrantBackingBean - grantTypeChange");
+
+
+    public void personNumberListener() {
+
+        System.out.println("RegisterGrantBackingBean - personNumberListener");
+
+        if(!(grant.getPersonNumber().equals(""))) {
+            person = GrantUtil.getPerson(grant.getPersonNumber());
+            issuedGrants = GrantUtil.getIssuedGrants(person.getPersonNumber());
+        }
+
     }
 
+    public void grantTypeListener(){
+        System.out.println("RegisterGrantBackingBean - grantTypeListener");
+    }
 
 
     public GrantVO getGrant() {
@@ -65,5 +86,21 @@ public class RegisterGrantBackingBean {
 
     public void setIssuedGrants(List<IssuedGrantVO> issuedGrants) {
         this.issuedGrants = issuedGrants;
+    }
+
+    public int getTabIndexActive() {
+        return tabIndexActive;
+    }
+
+    public void setTabIndexActive(int tabIndexActive) {
+        this.tabIndexActive = tabIndexActive;
+    }
+
+    public String getPortletNamespace() {
+        return portletNamespace;
+    }
+
+    public void setPortletNamespace(String portletNamespace) {
+        this.portletNamespace = portletNamespace;
     }
 }
--- core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/vo/GrantVO.java
+++ core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/vo/GrantVO.java
@@ -1,9 +1,16 @@
 package se.vgregion.glasogonbidrag.vo;
 
+import java.util.Date;
+
 public class GrantVO {
 
     private String grantType;
     private String personNumber;
+
+    private double amountExcludingVAT;
+    private Date prescriptionDate;
+    private Date receiptDate;
+
 
 
     public GrantVO() {
@@ -23,5 +30,29 @@ public class GrantVO {
 
     public void setPersonNumber(String personNumber) {
         this.personNumber = personNumber;
+    }
+
+    public double getAmountExcludingVAT() {
+        return amountExcludingVAT;
+    }
+
+    public void setAmountExcludingVAT(double amountExcludingVAT) {
+        this.amountExcludingVAT = amountExcludingVAT;
+    }
+
+    public Date getPrescriptionDate() {
+        return prescriptionDate;
+    }
+
+    public void setPrescriptionDate(Date prescriptionDate) {
+        this.prescriptionDate = prescriptionDate;
+    }
+
+    public Date getReceiptDate() {
+        return receiptDate;
+    }
+
+    public void setReceiptDate(Date receiptDate) {
+        this.receiptDate = receiptDate;
     }
 }
